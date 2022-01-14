fbee1854e354cf3cbc9f27b6dfe3309cdba4b65a
Victor Ross <victor2ross@gmail.com>
2022-01-13 21:51:38 -0300

    Primeiro commit

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,25 @@
+import { Flex } from "@chakra-ui/react";
+
+import { Header } from "../components/Header";
+import { Sidebar } from '../components/Sidebar';
+
+
+
+export default function Dashboard() {
+  return (
+    <Flex
+      direction="column"
+      height="100vh"
+    >
+      <Header />
+      <Flex
+        width="100%"
+        maxWidth={1480}
+        marginX="auto"
+        paddingX="6"
+      >
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
+}
